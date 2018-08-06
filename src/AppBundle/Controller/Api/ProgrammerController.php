f58002050d8f24c031de211f88aabea9c8235a0f
vtphan <vantruong.phan@agencedpc.fr>
2018-08-06 13:52:21 +0200

    feat: post, get creating and getting programmers

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -1,11 +1,13 @@
 <?php
 namespace AppBundle\Controller\Api;
 
+use AppBundle\AppBundle;
 use AppBundle\Controller\BaseController;
 use AppBundle\Entity\Programmer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -29,6 +31,66 @@ class ProgrammerController extends BaseController
         $em->persist($programmer);
         $em->flush();
 
-        return new Response($body);
+        $location = $this->generateUrl('api_programmers_show',[
+           'nickname' => $programmer->getNickname()
+        ]);
+        $data =  $this->serializeProgrammer($programmer);
+
+        $response = new JsonResponse($data,201);
+        $response->headers->set('Location',$location);
+
+        return $response;
     }
+
+
+    /**
+     * @Route("/api/programmers/{nickname}", name="api_programmers_show")
+     * @Method("GET")
+     */
+    public function showAction($nickname)
+    {
+        $programmer = $this->getDoctrine()
+            ->getRepository(Programmer::class)
+            ->findOneByNickname($nickname);
+        if(!$programmer){
+            throw $this->createNotFoundException('No programmer found for username '.$nickname);
+
+        }
+
+        $data  = $this->serializeProgrammer($programmer);
+        $response = new JsonResponse($data);
+        return $response;
+    }
+
+
+    /**
+     * @Route("/api/programmers")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $programmers = $this->getDoctrine()
+            ->getRepository(Programmer::class)
+            ->findAll();
+
+        $data = ['programmers' => []];
+        foreach ($programmers as $programmer){
+            $data['programmers'][] = $this->serializeProgrammer($programmer);
+        }
+
+        $response = new JsonResponse($data);
+
+        return $response;
+    }
+
+    private function serializeProgrammer(Programmer $programmer)
+    {
+        return [
+            'nickname' => $programmer->getNickname(),
+            'avatarNumber' => $programmer->getAvatarNumber(),
+            'powerLevel' => $programmer->getPowerLevel(),
+            'tagLine' => $programmer->getTagLine()
+        ];
+    }
+
 }
--- testing.php
+++ testing.php
@@ -14,9 +14,27 @@ $data = array(
     'avatarNumber' => 6,
     'tagLine' => 'a test dev'
 );
+
+// 1) POST to create the programmer
 $response = $client->post('/api/programmers',array(
     'body' => json_encode($data)
 ));
+
+echo $response;
+echo "\n\n";
+
+// 2) GET to fetch that programmer
+$programmerUrl = $response->getHeader('Location');
+echo $programmerUrl;
+
+$response = $client->get('/api/programmers/'.$nickName);
+
+echo $response;
+echo "\n\n";
+
+
+// 3) GET a collection
+$response = $client->get('/api/programmers');
 
 echo $response;
 echo "\n\n";
